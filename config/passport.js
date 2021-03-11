9b5d9ce19b124d4aaffa32d3b0fcce171e40aa69
Elvis Ciuffetelli <ciuffetellielvis@gmail.com>
2021-03-11 21:42:06 +0100

    added secret var to .env

--- config/passport.js
+++ config/passport.js
@@ -5,18 +5,18 @@ const User = mongoose.model("users");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = "secret";
-module.exports = passport => {
+opts.secretOrKey = process.env.SECRET_OR_KEY;
+module.exports = (passport) => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id)
-        .then(user => {
+        .then((user) => {
           if (user) {
             return done(null, user);
           }
           return done(null, false);
         })
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
     })
   );
 };
